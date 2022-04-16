fefc2a9596115a33ecfd07045a17a4bad2b8b99c
ani <atchonashvvili@yahoo.com>
2022-04-16 15:25:52 +0400

    Initial Commit

---
+++ appForGittConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appForGittConnect
+//
+//  Created by apple on 4/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
